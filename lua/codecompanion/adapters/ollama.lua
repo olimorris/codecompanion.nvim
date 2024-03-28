38dae8dc05cc84ca3295575bf5add5c1426035b1
Chaz Leong <13462818+cleong14@users.noreply.github.com>
2024-03-28 13:47:16 -1000

    chore: update ollama adapter

--- lua/codecompanion/adapters/ollama.lua
+++ lua/codecompanion/adapters/ollama.lua
@@ -99,6 +99,7 @@ return {
         "dolphin-mistral-7b-v2.6-dpo-laser-q4_K_M",
         "dolphin-mistral-code-companion",
         "dolphin-mistral-extract-wisdom",
+        "dolphin-mistral-extract-patterns",
         "dolphin-mistral-improve-writing",
         "dolphin-mistral-chat",
         "dolphin-mistral-code-comments",
@@ -129,6 +130,17 @@ return {
       desc = "What sampling temperature to use, between 0 and 2. Higher values like 0.8 will make the output more random, while lower values like 0.2 will make it more focused and deterministic. We generally recommend altering this or top_p but not both.",
       validate = function(n)
         return n >= 0 and n <= 2, "Must be between 0 and 2"
+      end,
+    },
+    top_p = {
+      order = 3,
+      mapping = "parameters.options",
+      type = "number",
+      optional = true,
+      default = 0.1,
+      desc = "An alternative to sampling with temperature, called nucleus sampling, where the model considers the results of the tokens with top_p probability mass. So 0.1 means only the tokens comprising the top 10% probability mass are considered. We generally recommend altering this or temperature but not both.",
+      validate = function(n)
+        return n >= 0 and n <= 1, "Must be between 0 and 1"
       end,
     },
   },
