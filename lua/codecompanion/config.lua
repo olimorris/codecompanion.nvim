6262ad578a17587ffb41f017994436ea50a6ab07
Oli Morris <olimorris@users.noreply.github.com>
2024-09-09 13:52:08 +0100

    fix: default configs

--- lua/codecompanion/config.lua
+++ lua/codecompanion/config.lua
@@ -48,7 +48,7 @@ return {
           description = "Share a loaded buffer's contents with the LLM",
           opts = {
             contains_code = true,
-            provider = "mini_pick", -- default|telescope|mini_pick|fzf_lua
+            provider = "default", -- default|telescope|mini_pick|fzf_lua
           },
         },
         ["file"] = {
@@ -57,7 +57,7 @@ return {
           opts = {
             contains_code = true,
             max_lines = 1000,
-            provider = "mini_pick", -- telescope|mini_pick|fzf_lua
+            provider = "telescope", -- telescope|mini_pick|fzf_lua
           },
         },
       },
