b91d032646ec6bb0f632799ab17e4aeddda3ec5a
Oli Morris <olimorris@users.noreply.github.com>
2025-01-26 09:57:21 +0000

    refactor(adapter): DeepSeek is more aligned with OpenAI

--- lua/codecompanion/adapters/deepseek.lua
+++ lua/codecompanion/adapters/deepseek.lua
@@ -1,4 +1,5 @@
 local log = require("codecompanion.utils.log")
+local openai = require("codecompanion.adapters.openai")
 
 ---Prepare data to be parsed as JSON
 ---@param data string | { body: string }
@@ -51,13 +52,12 @@ return {
       return true
     end,
 
-    ---Set the parameters
-    ---@param self CodeCompanion.Adapter
-    ---@param params table
-    ---@param messages table
-    ---@return table
+    --- Use the OpenAI adapter for the bulk of the work
+    tokens = function(self, data)
+      return openai.handlers.tokens(self, data)
+    end,
     form_parameters = function(self, params, messages)
-      return params
+      return openai.handlers.form_parameters(self, params, messages)
     end,
 
     ---Set the format of the role and content for the messages from the chat buffer
@@ -80,7 +80,7 @@ return {
         else
           table.insert(processed, {
             role = msg.role,
-            content = msg.content
+            content = msg.content,
           })
         end
       end
@@ -109,97 +109,14 @@ return {
       return { messages = processed }
     end,
 
-
-    ---Returns the number of tokens generated from the LLM
-    ---@param self CodeCompanion.Adapter
-    ---@param data table The data from the LLM
-    ---@return number|nil
-    tokens = function(self, data)
-      if data and data ~= "" then
-        local data_mod = prepare_data_for_json(data)
-        local ok, json = pcall(vim.json.decode, data_mod, { luanil = { object = true } })
-
-        if ok then
-          if json.usage then
-            local tokens = json.usage.total_tokens
-            log:trace("Tokens: %s", tokens)
-            return tokens
-          end
-        end
-      end
-    end,
-
-    ---Output the data from the API ready for insertion into the chat buffer
-    ---@param self CodeCompanion.Adapter
-    ---@param data table The streamed JSON data from the API, also formatted by the format_data handler
-    ---@return table|nil [status: string, output: table]
     chat_output = function(self, data)
-      local output = {}
-
-      if data and data ~= "" then
-        local data_mod = prepare_data_for_json(data)
-        local ok, json = pcall(vim.json.decode, data_mod, { luanil = { object = true } })
-
-        if ok and json.choices and #json.choices > 0 then
-          local choice = json.choices[1]
-          local delta = (self.opts and self.opts.stream) and choice.delta or choice.message
-
-          if delta then
-            if delta.role then
-              output.role = delta.role
-            else
-              output.role = nil
-            end
-
-            -- Some providers may return empty content
-            if delta.content then
-              output.content = delta.content
-            else
-              output.content = ""
-            end
-
-            return {
-              status = "success",
-              output = output,
-            }
-          end
-        end
-      end
+      return openai.handlers.chat_output(self, data)
     end,
-
-    ---Output the data from the API ready for inlining into the current buffer
-    ---@param self CodeCompanion.Adapter
-    ---@param data string|table The streamed JSON data from the API, also formatted by the format_data handler
-    ---@param context table Useful context about the buffer to inline to
-    ---@return string|table|nil
     inline_output = function(self, data, context)
-      if data and data ~= "" then
-        data = prepare_data_for_json(data)
-        local ok, json = pcall(vim.json.decode, data, { luanil = { object = true } })
-
-        if ok then
-          --- Some third-party OpenAI forwarding services may have a return package with an empty json.choices.
-          if not json.choices or #json.choices == 0 then
-            return
-          end
-
-          local choice = json.choices[1]
-          local delta = (self.opts and self.opts.stream) and choice.delta or choice.message
-          if delta.content then
-            return delta.content
-          end
-        end
-      end
+      return openai.handlers.inline_output(self, data, context)
     end,
-
-    ---Function to run when the request has completed. Useful to catch errors
-    ---@param self CodeCompanion.Adapter
-    ---@param data table
-    ---@return nil
     on_exit = function(self, data)
-      if data.status >= 400 then
-        log:error("Error: %s", data.body)
-      end
+      return openai.handlers.on_exit(self, data)
     end,
   },
   schema = {
@@ -232,8 +149,7 @@ return {
       type = "number",
       optional = true,
       default = 0.95,
-      desc =
-      "An alternative to sampling with temperature, called nucleus sampling, where the model considers the results of the tokens with top_p probability mass. So 0.1 means only the tokens comprising the top 10% probability mass are considered. We generally recommend altering this or temperature but not both. Not used for R1.",
+      desc = "An alternative to sampling with temperature, called nucleus sampling, where the model considers the results of the tokens with top_p probability mass. So 0.1 means only the tokens comprising the top 10% probability mass are considered. We generally recommend altering this or temperature but not both. Not used for R1.",
       validate = function(n)
         return n >= 0 and n <= 1, "Must be between 0 and 1"
       end,
@@ -258,8 +174,7 @@ return {
       type = "integer",
       optional = true,
       default = 8192,
-      desc =
-      "The maximum number of tokens to generate in the chat completion. The total length of input tokens and generated tokens is limited by the model's context length.",
+      desc = "The maximum number of tokens to generate in the chat completion. The total length of input tokens and generated tokens is limited by the model's context length.",
       validate = function(n)
         return n > 0, "Must be greater than 0"
       end,
@@ -270,8 +185,7 @@ return {
       type = "number",
       optional = true,
       default = 0,
-      desc =
-      "Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics. Not used for R1",
+      desc = "Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics. Not used for R1",
       validate = function(n)
         return n >= -2 and n <= 2, "Must be between -2 and 2"
       end,
@@ -282,8 +196,7 @@ return {
       type = "number",
       optional = true,
       default = 0,
-      desc =
-      "Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim. Not used for R1, but may be specified.",
+      desc = "Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim. Not used for R1, but may be specified.",
       validate = function(n)
         return n >= -2 and n <= 2, "Must be between -2 and 2"
       end,
@@ -294,8 +207,7 @@ return {
       type = "boolean",
       optional = true,
       default = nil,
-      desc =
-      "Whether to return log probabilities of the output tokens or not. If true, returns the log probabilities of each output token returned in the content of message. Not supported for R1.",
+      desc = "Whether to return log probabilities of the output tokens or not. If true, returns the log probabilities of each output token returned in the content of message. Not supported for R1.",
       subtype_key = {
         type = "integer",
       },
@@ -306,8 +218,7 @@ return {
       type = "string",
       optional = true,
       default = nil,
-      desc =
-      "A unique identifier representing your end-user, which can help OpenAI to monitor and detect abuse. Learn more.",
+      desc = "A unique identifier representing your end-user, which can help OpenAI to monitor and detect abuse. Learn more.",
       validate = function(u)
         return u:len() < 100, "Cannot be longer than 100 characters"
       end,
