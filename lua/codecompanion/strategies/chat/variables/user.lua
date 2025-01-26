c7015e3bd39a02d6b8b3f61459039d6b579da4ea
Oli Morris <olimorris@users.noreply.github.com>
2025-01-26 12:23:12 +0000

    fix: #698 custom variables

---
+++ lua/codecompanion/strategies/chat/variables/user.lua
@@ -0,0 +1,35 @@
+local buf_utils = require("codecompanion.utils.buffers")
+local config = require("codecompanion.config")
+
+---@class CodeCompanion.Variable.User: CodeCompanion.Variable
+local Variable = {}
+
+---@param args CodeCompanion.VariableArgs
+function Variable.new(args)
+  local self = setmetatable({
+    Chat = args.Chat,
+    config = args.config,
+    params = args.params,
+  }, { __index = Variable })
+
+  return self
+end
+
+---Return the user's custom variable
+---@return nil
+function Variable:output()
+  local id = "<var>" .. self.config.name .. "</var>"
+
+  self.Chat:add_message({
+    role = config.constants.USER_ROLE,
+    content = self.config.callback(),
+  }, { reference = id, tag = "variable", visible = false })
+
+  self.Chat.References:add({
+    bufnr = self.Chat.bufnr,
+    id = id,
+    source = "codecompanion.strategies.chat.variables.user",
+  })
+end
+
+return Variable
