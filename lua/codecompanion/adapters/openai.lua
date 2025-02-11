65016f384c495760524757b2d22a969a261a9179
Oli Morris <olimorris@users.noreply.github.com>
2025-02-11 18:42:03 +0000

    fix(openai): model streaming

--- lua/codecompanion/adapters/openai.lua
+++ lua/codecompanion/adapters/openai.lua
@@ -190,8 +190,9 @@ return {
       default = "gpt-4o",
       choices = {
         ["o3-mini-2025-01-31"] = { opts = { can_reason = true } },
-        ["o1-2024-12-17"] = { opts = { stream = true } },
-        ["o1-mini-2024-09-12"] = { opts = { can_reason = true } },
+        ["o1-2024-12-17"] = { opts = { stream = false } },
+        ["o1-preview"] = { opts = { stream = true } },
+        ["o1-mini-2024-09-12"] = { opts = { stream = true } },
         "gpt-4o",
         "gpt-4o-mini",
         "gpt-4-turbo-preview",
