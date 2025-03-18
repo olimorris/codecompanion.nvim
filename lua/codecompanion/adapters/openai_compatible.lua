c7c9165a1f4952caf772259ef31a5cc24aa3f2bd
Oli Morris <olimorris@users.noreply.github.com>
2025-03-18 09:17:45 +0000

    add openai compatible support

--- lua/codecompanion/adapters/openai_compatible.lua
+++ lua/codecompanion/adapters/openai_compatible.lua
@@ -2,64 +2,77 @@ local config = require("codecompanion.config")
 local curl = require("plenary.curl")
 local log = require("codecompanion.utils.log")
 local openai = require("codecompanion.adapters.openai")
+local utils = require("codecompanion.utils.adapters")
 
----Cached list of available models
----@type table<string>
-local _models = {}
+local _cache_expires
+local _cache_file = vim.fn.tempname()
+local _cached_models
+
+---Return the cached models
+---@params opts? table
+local function models(opts)
+  if opts and opts.last then
+    return _cached_models[1]
+  end
+  return _cached_models
+end
 
 ---Get a list of available OpenAI compatible models
 ---@params self CodeCompanion.Adapter
 ---@params opts? table
 ---@return table
 local function get_models(self, opts)
+  if _cached_models and _cache_expires > os.time() then
+    return models(opts)
+  end
+
+  _cached_models = {}
+
   local adapter = require("codecompanion.adapters").resolve(self)
   if not adapter then
     log:error("Could not resolve OpenAI compatible adapter in the `get_models` function")
     return {}
   end
 
-  if vim.tbl_isempty(_models) or config.display.chat.show_settings == false then
-    adapter:get_env_vars()
-    local url = adapter.env_replaced.url
-    local models_endpoint = adapter.env_replaced.models_endpoint
+  adapter:get_env_vars()
+  local url = adapter.env_replaced.url
+  local models_endpoint = adapter.env_replaced.models_endpoint
 
-    local headers = {
-      ["content-type"] = "application/json",
-    }
-    if adapter.env_replaced.api_key then
-      headers["Authorization"] = "Bearer " .. adapter.env_replaced.api_key
-    end
-
-    local ok, response, json
-
-    ok, response = pcall(function()
-      return curl.get(url .. models_endpoint, {
-        sync = true,
-        headers = headers,
-        insecure = config.adapters.opts.allow_insecure,
-        proxy = config.adapters.opts.proxy,
-      })
-    end)
-    if not ok then
-      log:error("Could not get the OpenAI compatible models from " .. url .. "/v1/models.\nError: %s", response)
-      return {}
-    end
-
-    ok, json = pcall(vim.json.decode, response.body)
-    if not ok then
-      log:error("Could not parse the response from " .. url .. "/v1/models")
-      return {}
-    end
-
-    for _, model in ipairs(json.data) do
-      table.insert(_models, model.id)
-    end
+  local headers = {
+    ["content-type"] = "application/json",
+  }
+  if adapter.env_replaced.api_key then
+    headers["Authorization"] = "Bearer " .. adapter.env_replaced.api_key
   end
 
-  if opts and opts.last then
-    return _models[1]
+  local ok, response, json
+
+  ok, response = pcall(function()
+    return curl.get(url .. models_endpoint, {
+      sync = true,
+      headers = headers,
+      insecure = config.adapters.opts.allow_insecure,
+      proxy = config.adapters.opts.proxy,
+    })
+  end)
+  if not ok then
+    log:error("Could not get the OpenAI compatible models from " .. url .. "/v1/models.\nError: %s", response)
+    return {}
   end
-  return _models
+
+  ok, json = pcall(vim.json.decode, response.body)
+  if not ok then
+    log:error("Could not parse the response from " .. url .. "/v1/models")
+    return {}
+  end
+
+  for _, model in ipairs(json.data) do
+    table.insert(_cached_models, model.id)
+  end
+
+  _cache_expires = utils.refresh_cache(_cache_file, config.adapters.opts.cache_for)
+
+  return models(opts)
 end
 
 ---@class OpenAICompatible.Adapter: CodeCompanion.Adapter
