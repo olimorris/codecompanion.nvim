2095f69c8495db60099da8fdfbd98329f657dc1a
Adam Macumber <adam@macumber.io>
2025-03-24 15:37:09 -0400

    feat: Allow marking "empty" tables to be encoded as empty objects.

--- lua/codecompanion/utils/json.lua
+++ lua/codecompanion/utils/json.lua
@@ -3,7 +3,11 @@ local M = {}
 ---@param data any
 ---@return string
 M.encode = function(data)
-  return vim.json.encode(data)
+  local output = vim.json.encode(data)
+  -- Replace instances where an empty array should actually be an empty object.
+  -- Specify an empty object by adding a single entry of "empty_object" to
+  -- the table.
+  return output:gsub('%["__empty_object__"%]', "{}")
 end
 
 ---Decode a yaml node
