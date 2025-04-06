9957437efaab4a30ec6f5ea8253314dc21dd177c
Oli Morris <olimorris@users.noreply.github.com>
2025-04-06 12:55:40 +0100

    fix(chat): `:CodeCompanionChat` now works

--- lua/codecompanion/strategies/chat/init.lua
+++ lua/codecompanion/strategies/chat/init.lua
@@ -488,7 +488,7 @@ end
 local function has_tag(tag, messages)
   return vim.tbl_contains(
     vim.tbl_map(function(msg)
-      return msg.opts.tag
+      return msg.opts and msg.opts.tag
     end, messages),
     tag
   )
