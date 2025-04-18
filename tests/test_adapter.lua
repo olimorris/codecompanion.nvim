3cc2c4aa3fcbef5af1bb43641a7ca07ef0589846
Oli Morris <olimorris@users.noreply.github.com>
2025-04-19 00:25:59 +0100

    update adapter test

--- tests/test_adapter.lua
+++ tests/test_adapter.lua
@@ -90,119 +90,166 @@ local test_adapter2 = {
   },
 }
 
-describe("Adapter", function()
-  it("can form parameters from a chat buffer's settings", function()
-    local adapter = require("codecompanion.adapters").extend("openai")
-    local result = adapter:map_schema_to_params(chat_buffer_settings)
+local new_set = MiniTest.new_set
+local T = new_set()
 
-    -- Ignore this for now
-    result.parameters.stream = nil
-    result.parameters.stream_options = nil
+T["Adapter"] = new_set()
+T["Adapter"]["can form parameters from a chat buffer's settings"] = function()
+  local adapter = require("codecompanion.adapters").extend("openai")
+  local result = adapter:map_schema_to_params(chat_buffer_settings)
 
-    h.eq(chat_buffer_settings, result.parameters)
-  end)
+  -- Ignore this for now
+  result.parameters.stream = nil
+  result.parameters.stream_options = nil
 
-  it("can nest parameters based on an adapter's schema", function()
-    local adapter = require("codecompanion.adapters").extend(test_adapter)
-    local result = adapter:map_schema_to_params(chat_buffer_settings)
+  h.eq(chat_buffer_settings, result.parameters)
+end
 
-    local expected = {
-      stream = true,
-      data = {
-        model = "gpt-4-0125-preview",
+T["Adapter"]["can nest parameters based on an adapter's schema"] = function()
+  local adapter = require("codecompanion.adapters").extend(test_adapter)
+  local result = adapter:map_schema_to_params(chat_buffer_settings)
+
+  local expected = {
+    stream = true,
+    data = {
+      model = "gpt-4-0125-preview",
+    },
+    options = {
+      temperature = 1,
+      top_p = 1,
+    },
+  }
+
+  h.eq(expected, result.parameters)
+end
+
+T["Adapter"]["can form environment variables"] = function()
+  local adapter = require("codecompanion.adapters").extend(test_adapter2)
+  local result = adapter:get_env_vars()
+
+  h.eq(test_adapter2.schema.model.default, result.env_replaced.model)
+  h.eq(os.getenv("HOME"), result.env_replaced.home)
+end
+
+T["Adapter"]["can set environment variables in the adapter"] = function()
+  local adapter = require("codecompanion.adapters").extend(test_adapter2)
+  adapter:get_env_vars()
+
+  local url = adapter:set_env_vars(adapter.url)
+  h.eq("https://api.oli.ai/v1/chat/oli_model_v2", url)
+
+  local headers = adapter:set_env_vars(adapter.headers)
+  h.eq({
+    content_type = "application/json",
+    home = os.getenv("HOME"),
+  }, headers)
+end
+
+T["Adapter"]["will not set environment variables if it doesn't need to"] = function()
+  local adapter = require("codecompanion.adapters").extend(test_adapter2)
+  adapter:get_env_vars()
+
+  local params = adapter:set_env_vars(adapter.parameters)
+  h.eq(test_adapter2.parameters, params)
+end
+
+T["Adapter"]["can consolidate consecutive messages"] = function()
+  local messages = {
+    { role = "system", content = "This is a system prompt" },
+    { role = "user", content = "Foo" },
+    { role = "user", content = "Bar" },
+  }
+
+  h.eq({
+    { role = "system", content = "This is a system prompt" },
+    { role = "user", content = "Foo\n\nBar" },
+  }, utils.merge_messages(messages))
+end
+
+T["Adapter"]["can smartly merge tables together"] = function()
+  local messages = {
+    {
+      role = "user",
+      content = {
+        content = "Foo",
+        tool_id = "123",
       },
-      options = {
-        temperature = 1,
-        top_p = 1,
+    },
+    {
+      role = "user",
+      content = {
+        content = "Bar",
+        tool_id = "456",
       },
-    }
+    },
+    {
+      role = "assistant",
+      content = "Foobar ftw!",
+    },
+  }
 
-    h.eq(expected, result.parameters)
-  end)
+  h.eq({
+    {
+      content = {
+        {
+          content = "Foo",
+          tool_id = "123",
+        },
+        {
+          content = "Bar",
+          tool_id = "456",
+        },
+      },
+      role = "user",
+    },
+    {
+      content = "Foobar ftw!",
+      role = "assistant",
+    },
+  }, utils.merge_messages(messages))
+end
 
-  it("can form environment variables", function()
-    local adapter = require("codecompanion.adapters").extend(test_adapter2)
-    local result = adapter:get_env_vars()
+T["Adapter"]["can consolidate system messages"] = function()
+  local messages = {
+    { role = "system", content = "This is a system prompt" },
+    { role = "user", content = "Foo" },
+    { role = "assistant", content = "Bar" },
+    { role = "system", content = "This is ANOTHER system prompt" },
+    { role = "user", content = "Baz" },
+  }
 
-    h.eq(test_adapter2.schema.model.default, result.env_replaced.model)
-    h.eq(os.getenv("HOME"), result.env_replaced.home)
-  end)
+  h.eq({
+    { role = "system", content = "This is a system prompt This is ANOTHER system prompt" },
+    { role = "user", content = "Foo" },
+    { role = "assistant", content = "Bar" },
+    { role = "user", content = "Baz" },
+  }, utils.merge_system_messages(messages))
+end
 
-  it("can set environment variables in the adapter", function()
-    local adapter = require("codecompanion.adapters").extend(test_adapter2)
-    adapter:get_env_vars()
+T["Adapter"]["can be used to remove groups of messages"] = function()
+  local messages = {
+    { role = "system", content = "This is a system prompt" },
+    { role = "system", content = "This is another system prompt" },
+    { role = "user", content = "Foo" },
+    { role = "user", content = "Bar" },
+  }
 
-    local url = adapter:set_env_vars(adapter.url)
-    h.eq("https://api.oli.ai/v1/chat/oli_model_v2", url)
-
-    local headers = adapter:set_env_vars(adapter.headers)
-    h.eq({
-      content_type = "application/json",
-      home = os.getenv("HOME"),
-    }, headers)
-  end)
-
-  it("will not set environment variables if it doesn't need to", function()
-    local adapter = require("codecompanion.adapters").extend(test_adapter2)
-    adapter:get_env_vars()
-
-    local params = adapter:set_env_vars(adapter.parameters)
-    h.eq(test_adapter2.parameters, params)
-  end)
-
-  it("can consolidate consecutive messages", function()
-    local messages = {
-      { role = "system", content = "This is a system prompt" },
-      { role = "user", content = "Foo" },
-      { role = "user", content = "Bar" },
-    }
-
-    h.eq({
-      { role = "system", content = "This is a system prompt" },
-      { role = "user", content = "Foo\n\nBar" },
-    }, utils.merge_messages(messages))
-  end)
-
-  it("can consolidate system messages", function()
-    local messages = {
-      { role = "system", content = "This is a system prompt" },
-      { role = "user", content = "Foo" },
-      { role = "assistant", content = "Bar" },
-      { role = "system", content = "This is ANOTHER system prompt" },
-      { role = "user", content = "Baz" },
-    }
-
-    h.eq({
-      { role = "system", content = "This is a system prompt This is ANOTHER system prompt" },
-      { role = "user", content = "Foo" },
-      { role = "assistant", content = "Bar" },
-      { role = "user", content = "Baz" },
-    }, utils.merge_system_messages(messages))
-  end)
-
-  it("can be used to remove groups of messages", function()
-    local messages = {
-      { role = "system", content = "This is a system prompt" },
-      { role = "system", content = "This is another system prompt" },
-      { role = "user", content = "Foo" },
-      { role = "user", content = "Bar" },
-    }
-
-    -- Taken directly from the Anthropic adapter
-    local sys_prompt = utils.get_msg_index("system", messages)
-    if sys_prompt and #sys_prompt > 0 then
-      -- Sort the prompts in descending order so we can remove them from the table without shifting indexes
-      table.sort(sys_prompt, function(a, b)
-        return a > b
-      end)
-      for _, prompt in ipairs(sys_prompt) do
-        table.remove(messages, prompt)
-      end
+  -- Taken directly from the Anthropic adapter
+  local sys_prompt = utils.get_msg_index("system", messages)
+  if sys_prompt and #sys_prompt > 0 then
+    -- Sort the prompts in descending order so we can remove them from the table without shifting indexes
+    table.sort(sys_prompt, function(a, b)
+      return a > b
+    end)
+    for _, prompt in ipairs(sys_prompt) do
+      table.remove(messages, prompt)
     end
+  end
 
-    h.eq({
-      { role = "user", content = "Foo" },
-      { role = "user", content = "Bar" },
-    }, messages)
-  end)
-end)
+  h.eq({
+    { role = "user", content = "Foo" },
+    { role = "user", content = "Bar" },
+  }, messages)
+end
+
+return T
