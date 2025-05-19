bddbd6cb2b0bbfb04d988073c8fd06a93577abe9
Oli Morris <olimorris@users.noreply.github.com>
2025-05-19 09:11:22 +0100

    add default provider

--- lua/codecompanion/strategies/chat/slash_commands/image.lua
+++ lua/codecompanion/strategies/chat/slash_commands/image.lua
@@ -1,69 +1,14 @@
 local Curl = require("plenary.curl")
-local Job = require("plenary.job")
+local base64 = require("codecompanion.utils.base64")
 local config = require("codecompanion.config")
 local util = require("codecompanion.utils")
 
-local dirs = { vim.fn.getcwd() } -- Always search the cwd for images
-
 local CONSTANTS = {
   NAME = "Image",
-  PROMPT = "Select an image(s)",
+  PROMPT = "Images",
   IMAGE_DIRS = config.strategies.chat.slash_commands.image.opts.dirs,
   IMAGE_TYPES = config.strategies.chat.slash_commands.image.opts.filetypes,
 }
-
----Encode a given file to base64
----@param filepath string The path to the file to encode.
----@return string?, string?
-local function get_base64_from_file(filepath)
-  if vim.fn.executable("base64") == 0 then
-    return nil, "Could not find the `base64` command."
-  end
-
-  local args
-  local uname_info = vim.loop.os_uname()
-  if uname_info and uname_info.sysname == "Darwin" then
-    args = { "-i", filepath }
-  elseif uname_info and uname_info.sysname == "Linux" then
-    args = { "-w", "0", filepath }
-  else
-    args = { filepath }
-  end
-
-  local job = Job:new({
-    command = "base64",
-    args = args,
-    enable_recording = true,
-  })
-
-  local sync_ok, sync_payload = pcall(function()
-    job:sync(5000)
-  end)
-
-  if not sync_ok then
-    return nil, "base64 encoding failed or timed out: " .. tostring(sync_payload)
-  end
-
-  if job.code == 0 then
-    local stdout_results = job:result()
-    local b64_content = nil
-    if stdout_results and #stdout_results > 0 then
-      b64_content = table.concat(stdout_results, "")
-      b64_content = vim.trim(b64_content)
-    end
-    if b64_content and #b64_content > 0 then
-      return b64_content, nil
-    else
-      return nil, "base64 encoding produced empty output."
-    end
-  else
-    local stderr_msg = ""
-    if job:stderr_result() and #(job:stderr_result()) > 0 then
-      stderr_msg = ": " .. table.concat(job:stderr_result(), " ")
-    end
-    return nil, "Could not base64 encode image (code " .. tostring(job.code) .. ")" .. stderr_msg
-  end
-end
 
 ---Get the mimetype from the given file
 ---@param filepath string The path to the file
@@ -83,11 +28,30 @@ local function get_mimetype(filepath)
   return map[extension]
 end
 
+---Prepares image search directories and filetypes for a single invocation.
+---@return table, table|nil Returns search_dirs and filetypes
+local function prepare_image_search_options()
+  local current_search_dirs = { vim.fn.getcwd() } -- Start with CWD for this call
+
+  if CONSTANTS.IMAGE_DIRS and vim.tbl_count(CONSTANTS.IMAGE_DIRS) > 0 then
+    vim.list_extend(current_search_dirs, CONSTANTS.IMAGE_DIRS)
+  end
+
+  local ft = nil
+  if CONSTANTS.IMAGE_TYPES and vim.tbl_count(CONSTANTS.IMAGE_TYPES) > 0 then
+    ft = CONSTANTS.IMAGE_TYPES
+  end
+
+  return current_search_dirs, ft
+end
+
 local providers = {
   ---The default provider
   ---@param SlashCommand CodeCompanion.SlashCommand
   ---@return nil
   default = function(SlashCommand)
+    local dirs, ft = prepare_image_search_options()
+
     local default = require("codecompanion.providers.slash_commands.default")
     default = default
       .new({
@@ -97,7 +61,7 @@ local providers = {
         SlashCommand = SlashCommand,
         title = CONSTANTS.PROMPT,
       })
-      :buffers()
+      :images(dirs, ft)
       :display()
   end,
 
@@ -107,7 +71,6 @@ local providers = {
   snacks = function(SlashCommand)
     local snacks = require("codecompanion.providers.slash_commands.snacks")
     snacks = snacks.new({
-      title = CONSTANTS.PROMPT .. ": ",
       output = function(selection)
         return SlashCommand:output({
           relative_path = selection.file,
@@ -116,21 +79,13 @@ local providers = {
       end,
     })
 
-    if CONSTANTS.IMAGE_DIRS and vim.tbl_count(CONSTANTS.IMAGE_DIRS) > 0 then
-      vim.list_extend(dirs, CONSTANTS.IMAGE_DIRS)
-    end
-
-    local ft = nil
-    if CONSTANTS.IMAGE_TYPES and vim.tbl_count(CONSTANTS.IMAGE_TYPES) > 0 then
-      ft = CONSTANTS.IMAGE_TYPES
-    end
+    local dirs, ft = prepare_image_search_options()
 
     snacks.provider.picker.pick("files", {
       confirm = snacks:display(),
       dirs = dirs,
       ft = ft,
       main = { file = false, float = true },
-      prompt = snacks.title,
     })
   end,
 }
@@ -241,7 +196,7 @@ end
 function SlashCommand:output(selected, opts)
   local id = "<image>" .. (selected.id or selected.path) .. "</image>"
 
-  local b64_content, b64_err = get_base64_from_file(selected.path)
+  local b64_content, b64_err = base64.encode(selected.path)
   if b64_err then
     return util.notify(b64_err, vim.log.levels.ERROR)
   end
