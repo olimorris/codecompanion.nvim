c184a1d1d120b509c5ec078e91e3a47435c3ac89
Oli Morris <olimorris@users.noreply.github.com>
2025-05-22 22:31:54 +0100

    make web search use a child process

--- tests/strategies/chat/agents/tools/test_web_search.lua
+++ tests/strategies/chat/agents/tools/test_web_search.lua
@@ -1,46 +1,52 @@
-local new_set = MiniTest.new_set
 local h = require("tests.helpers")
 
-local client = require("codecompanion.http")
-local tavily = require("codecompanion.adapters.tavily")
-local web_search = require("codecompanion.strategies.chat.agents.tools.web_search")
+local new_set = MiniTest.new_set
 
+local child = MiniTest.new_child_neovim()
 local T = new_set({
   hooks = {
     pre_case = function()
-      T._original_client = client.new
-      client.new = function(_)
-        return {
-          request = function(_, _, opts)
-            local lines = vim.fn.readfile("tests/adapters/stubs/output/tavily_search.txt")
-            local json_string = table.concat(lines, "\n")
-            local tavily_search_stub = vim.json.decode(json_string)
-            opts.callback(nil, tavily_search_stub)
-          end,
-        }
-      end
-    end,
-
-    post_case = function()
-      client.new = T._original_client
+      child.restart({ "-u", "scripts/minimal_init.lua" })
+      child.o.statusline = ""
+      child.o.laststatus = 0
     end,
   },
 })
 
 T["web search tool"] = function()
-  local self = {
-    tool = tavily,
-  }
+  local result = child.lua([[
+    local client = require("codecompanion.http")
+    client.new = function(_)
+      return {
+        request = function(_, _, opts)
+          local lines = vim.fn.readfile("tests/adapters/stubs/output/tavily_search.txt")
+          local json_string = table.concat(lines, "\n")
+          local tavily_search_stub = vim.json.decode(json_string)
+          opts.callback(nil, tavily_search_stub)
+        end,
+      }
+    end
 
-  local args = {
-    query = "Neovim latest version release May 2025",
-  }
+    local tavily = require("codecompanion.adapters.tavily")
+    local web_search = require("codecompanion.strategies.chat.agents.tools.web_search")
 
-  local callback = function(result)
-    h.eq(result.status, "success")
-  end
+    local self = {
+      tool = tavily,
+    }
+    local args = {
+      query = "Neovim latest version release May 2025",
+    }
 
-  web_search.cmds[1](self, args, nil, callback)
+    local output
+
+    web_search.cmds[1](self, args, nil, function(result)
+      output = result.status
+    end)
+
+    return output
+  ]])
+
+  h.eq("success", result)
 end
 
 return T
