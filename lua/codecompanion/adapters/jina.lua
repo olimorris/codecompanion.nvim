d39890cb432801cfe5b2a97104289e1cc4b9c415
Oli <oli.morris@hey.com>
2025-05-27 19:30:48 +0100

    feat(slash_commands): better caching and restoring of URLs (#1491)

--- lua/codecompanion/adapters/jina.lua
+++ lua/codecompanion/adapters/jina.lua
@@ -4,20 +4,38 @@ return {
   opts = {
     stream = false,
   },
-  url = "https://r.jina.ai",
-  headers = {
-    ["Content-Type"] = "application/json",
-    ["X-Return-Format"] = "text",
-    ["Accept"] = "application/json",
-  },
+  url = "",
+  headers = {},
   schema = {
     model = {
       default = "jina",
     },
   },
-  handlers = {
-    set_body = function(self, data)
-      return { url = data.url }
-    end,
+  handlers = {},
+  methods = {
+    slash_commands = {
+      ---@param self CodeCompanion.Adapter
+      ---@param data table
+      ---@return {data: table, status: string}|nil
+      fetch = function(self, data)
+        self.url = "https://r.jina.ai"
+
+        self.handlers.set_body = function(s, d)
+          return { url = d.url }
+        end
+
+        self.headers = vim.tbl_deep_extend("force", self.headers, {
+          ["Content-Type"] = "application/json",
+          ["X-Return-Format"] = "text",
+          ["Accept"] = "application/json",
+        })
+
+        if self.env and self.env.api_key then
+          self.headers = vim.tbl_deep_extend("force", self.headers, {
+            ["Authorization"] = "Bearer ${api_key}",
+          })
+        end
+      end,
+    },
   },
 }
