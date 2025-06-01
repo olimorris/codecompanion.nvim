26a032e6bc32f88512236852c2bfb4dfb9948498
Oli Morris <olimorris@users.noreply.github.com>
2025-06-01 22:16:26 +0100

    clean up minimal_init

--- scripts/minimal_init.lua
+++ scripts/minimal_init.lua
@@ -17,15 +17,7 @@ require("nvim-treesitter")
     "yaml",
   })
   :wait(300000)
-
 vim.treesitter.language.register("markdown", "codecompanion")
 
--- Set up 'mini.test' only when calling headless Neovim (like with `make test`)
-if #vim.api.nvim_list_uis() == 0 then
-  -- Add 'mini.nvim' to 'runtimepath' to be able to use 'mini.test'
-  -- Assumed that 'mini.nvim' is stored in 'deps/mini.nvim'
-  vim.cmd("set rtp+=deps/mini.nvim")
-
-  -- Set up 'mini.test'
-  require("mini.test").setup()
-end
+vim.cmd("set rtp+=deps/mini.nvim")
+require("mini.test").setup()
