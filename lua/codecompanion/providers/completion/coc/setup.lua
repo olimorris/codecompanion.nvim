78bc9d3a551cddd19d87ba6c3a4984749b0383af
Pavel Hajek <p135246@gmail.com>
2025-06-01 15:32:31 +0200

    fix(coc): forgotten execute function registration

--- lua/codecompanion/providers/completion/coc/setup.lua
+++ lua/codecompanion/providers/completion/coc/setup.lua
@@ -2,6 +2,8 @@ local cocCompletion = require("codecompanion.providers.completion.coc")
 
 _G.codecompanion_coc_init = cocCompletion.init
 _G.codecompanion_coc_complete = cocCompletion.complete
+_G.codecompanion_coc_execute = cocCompletion.execute
+
 vim.api.nvim_create_autocmd("VimEnter", {
   callback = cocCompletion.ensure_autoload_file,
 })
