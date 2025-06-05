427ffc767b64918902751d0ee9b510a65f8064dc
Oli Morris <olimorris@users.noreply.github.com>
2025-06-05 23:43:39 +0100

    remove redundant test

--- scripts/minimal_init.lua
+++ scripts/minimal_init.lua
@@ -10,24 +10,16 @@ vim.cmd("set rtp+=./deps/nvim-treesitter")
 require("nvim-treesitter").setup({
   install_dir = "deps/parsers",
 })
-
-local ensure_installed = {
-  "go",
-  "lua",
-  "markdown",
-  "markdown_inline",
-  "python",
-  "yaml",
-}
-
-local installed = require("nvim-treesitter.config").installed_parsers()
-local not_installed = vim.tbl_filter(function(parser)
-  return not vim.tbl_contains(installed, parser)
-end, ensure_installed)
-
-if #not_installed > 0 then
-  require("nvim-treesitter").install(not_installed):wait(300000)
-end
+require("nvim-treesitter")
+  .install({
+    "go",
+    "lua",
+    "markdown",
+    "markdown_inline",
+    "python",
+    "yaml",
+  })
+  :wait(300000)
 
 vim.treesitter.language.register("markdown", "codecompanion")
 
