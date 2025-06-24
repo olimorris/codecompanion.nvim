2ea49ff2063100757e71b0e327b7a32a1d566a9d
Oli <oli.morris@hey.com>
2025-06-24 22:46:11 +0100

    feat(workspaces): files can be opened as buffers (#1708)
    Co-authored-by: Oli Morris <olimorris@users.noreply.github.com>

--- tests/strategies/chat/slash_commands/test_workspace.lua
+++ tests/strategies/chat/slash_commands/test_workspace.lua
@@ -97,6 +97,20 @@ T["Workspace"]["files and symbols are added to the chat"] = function()
   )
 end
 
+T["Workspace"]["can open a file as a buffer if it's already open"] = function()
+  child.lua([[
+    -- Comment this out and see that it's loaded as a file instead
+    vim.cmd("edit tests/stubs/stub.go")
+
+    _G.set_workspace()
+    _G.wks:output("Test")
+  ]])
+
+  local messages = child.lua_get([[_G.chat.messages]])
+
+  h.expect_starts_with('<attachment filepath="tests/stubs/stub.go" buffer_number=', messages[5].content)
+end
+
 T["Workspace"]["top-level prompts are not duplicated and are ordered correctly"] = function()
   workspace_json = vim.json.decode(table.concat(vim.fn.readfile("tests/stubs/workspace_multiple.json"), ""))
 
