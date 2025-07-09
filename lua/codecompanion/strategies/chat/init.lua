2f4c9713675dca7bec7ce3d077192b40f86c80f1
Oli Morris <olimorris@users.noreply.github.com>
2025-07-09 23:42:40 +0100

    fix chat:submit

--- lua/codecompanion/strategies/chat/init.lua
+++ lua/codecompanion/strategies/chat/init.lua
@@ -956,16 +956,22 @@ function Chat:submit(opts)
           self.status = result.status
           if self.status == CONSTANTS.STATUS_SUCCESS then
             if result.output.role then
-              self._last_role = result.output.role
               result.output.role = config.constants.LLM_ROLE
+              self._last_role = config.constants.LLM_ROLE
             end
             if result.output.reasoning then
               table.insert(reasoning, result.output.reasoning)
-              self:add_buf_message(result.output.reasoning, { type = self.MESSAGE_TYPES.REASONING_MESSAGE })
+              self:add_buf_message(
+                { role = config.constants.LLM_ROLE, content = result.output.reasoning.content },
+                { type = self.MESSAGE_TYPES.REASONING_MESSAGE }
+              )
             end
             if result.output.content then
               table.insert(output, result.output.content)
-              self:add_buf_message(result.output, { type = self.MESSAGE_TYPES.LLM_MESSAGE })
+              self:add_buf_message(
+                { role = config.constants.LLM_ROLE, content = result.output.content },
+                { type = self.MESSAGE_TYPES.LLM_MESSAGE }
+              )
             end
           elseif self.status == CONSTANTS.STATUS_ERROR then
             log:error("Error: %s", result.output)
