7ca2c08a2a8606d64202b8d81a022a05c3049d5f
Zhe Yu <zcabzyu@ucl.ac.uk>
2025-07-14 21:00:30 +0800

    tests(ollama): add image message test

--- tests/adapters/test_ollama.lua
+++ tests/adapters/test_ollama.lua
@@ -133,6 +133,54 @@ T["Ollama adapter"]["Streaming"]["can output streamed data into the chat buffer"
   h.eq("**Dynamic Object-Oriented**", output)
 end
 
+T["Ollama adapter"]["Streaming"]["can form messages with images"] = function()
+  local messages = {
+    {
+      content = "How are you?",
+      role = "user",
+    },
+    {
+      content = "I am fine, thanks. How can I help?",
+      role = "assistant",
+    },
+    {
+      content = "somefakebase64encoding",
+      role = "user",
+      opts = {
+        mimetype = "image/jpg",
+        reference = "<image>https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg</image>",
+        tag = "image",
+        visible = false,
+      },
+    },
+    {
+      content = "What is this an image of?",
+      role = "user",
+    },
+  }
+
+  local expected = {
+    {
+      content = "How are you?",
+      role = "user",
+    },
+    {
+      content = "I am fine, thanks. How can I help?",
+      role = "assistant",
+    },
+    {
+      images = { "somefakebase64encoding" },
+      role = "user",
+    },
+    {
+      content = "What is this an image of?",
+      role = "user",
+    },
+  }
+
+  h.eq(expected, adapter.handlers.form_messages(adapter, messages).messages)
+end
+
 T["Ollama adapter"]["Streaming"]["can process tools"] = function()
   local tools = {}
   local lines = vim.fn.readfile("tests/adapters/stubs/ollama_tools_streaming.txt")
