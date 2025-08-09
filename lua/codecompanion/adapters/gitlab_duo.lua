528f134c1f573d53e6f137127d2322b29a48cfc9
Kraust <secretdragoon@gmail.com>
2025-08-08 21:00:38 -0400

    Added Gitlab Duo Adapter.

---
+++ lua/codecompanion/adapters/gitlab_duo.lua
@@ -0,0 +1,60 @@
+---@class GitlabDuo.Adapter: CodeCompanion.Adapter
+return {
+    name = "gitlab_duo",
+    formatted_name = "Gitlab Duo",
+    roles = {
+        llm = "assistant",
+        user = "user",
+    },
+    opts = {
+        stream = true,
+        vision = false,
+    },
+    features = {
+        text = true,
+        tokens = false,
+    },
+    url = "${url}${chat_url}",
+    env = {
+        api_key = "GITLAB_API_KEY",
+        url = "GITLAB_URL",
+        chat_url = "/api/v4/chat/completions",
+    },
+    headers = {
+        Authorization = "Bearer ${api_key}",
+        ["Content-Type"] = "application/json",
+    },
+    handlers = {
+        form_messages = function(self, messages)
+            -- messages must be shorter than 1000 characters.
+            -- This issues with the default system_prompt.
+            local merged_content = "";
+            for _, message in ipairs(messages) do
+                merged_content = merged_content .. message.content
+            end
+            return { content = merged_content }
+        end,
+        chat_output = function(self, data, tools)
+            local ok, body = pcall(vim.json.decode, data.body)
+            if not ok then
+                return {
+                    status = "error",
+                    output = "Could not parse JSON response",
+                }
+            end
+            if data and data.status >= 400 then
+                return {
+                    status = "error",
+                    output = body.error,
+                }
+            end
+            return {
+                status = "success",
+                output = {
+                    role = "assistant",
+                    content = body,
+                }
+            }
+        end,
+    }
+}
