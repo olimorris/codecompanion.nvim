478c1cea6e2d55ad149d20a71e30cc91524de434
Oli Morris <olimorris@users.noreply.github.com>
2025-08-17 21:11:43 +0100

    refactor: rename `inline` to be `inline_default`

--- lua/codecompanion/providers/init.lua
+++ lua/codecompanion/providers/init.lua
@@ -21,9 +21,9 @@ local configs = {
   },
 
   -- Diffs
-  inline = {
-    module = "codecompanion.providers.diff.inline", -- Internal module path
-    name = "inline",
+  inline_default = {
+    module = "codecompanion.providers.diff.inline_default", -- Internal module path
+    name = "inline_default",
     -- No condition needed - always available since it's internal
   },
   mini_diff = {
@@ -92,8 +92,8 @@ end
 ---Get the default Diff provider
 ---@return string
 local function diff_providers()
-  local providers = { "inline", "mini_diff" }
-  return find_provider(providers, configs, "default")
+  local providers = { "default", "mini_diff" }
+  return find_provider(providers, configs, "inline_default")
 end
 
 ---Get the default Completion provider
