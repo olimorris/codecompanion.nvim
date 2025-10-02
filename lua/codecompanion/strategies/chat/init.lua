f20eb4fbbf2c1008bc4a8c519fd04791279067f7
Oli Morris <olimorris@users.noreply.github.com>
2025-10-02 10:09:09 +0100

    fix: use correct config item

--- lua/codecompanion/strategies/chat/init.lua
+++ lua/codecompanion/strategies/chat/init.lua
@@ -361,7 +361,7 @@ function Chat.new(args)
       local bufnr = api.nvim_create_buf(false, true)
       api.nvim_buf_set_name(bufnr, fmt("[CodeCompanion] %d", id))
       api.nvim_set_option_value("filetype", "codecompanion", { buf = bufnr })
-      api.nvim_set_option_value("undolevels", config.strategies.chat.opts.undolevels or 10, { buf = bufnr })
+      api.nvim_set_option_value("undolevels", config.strategies.chat.opts.undo_levels or 10, { buf = bufnr })
 
       -- Safely attach treesitter
       vim.schedule(function()
