ed4d7f293e8374b3b025f25fff9bced2e1d45a3d
Oli Morris <olimorris@users.noreply.github.com>
2025-12-14 18:28:42 +0000

    clean up rtp

--- scripts/minimal_init.lua
+++ scripts/minimal_init.lua
@@ -1,7 +1,7 @@
 vim.cmd([[let &rtp.=','.getcwd()]])
 vim.cmd("set rtp+=deps/mini.nvim")
-vim.cmd("set rtp+=./deps/plenary.nvim")
-vim.cmd("set rtp+=./deps/nvim-treesitter")
+vim.cmd("set rtp+=deps/plenary.nvim")
+vim.cmd("set rtp+=deps/nvim-treesitter")
 
 -- Ensure mini.test is available
 require("mini.test").setup()
