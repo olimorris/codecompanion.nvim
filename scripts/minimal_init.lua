43307e45aab7d9a67a9aad94cabb5e2c0c2be48a
Oli Morris <olimorris@users.noreply.github.com>
2025-12-14 18:23:11 +0000

    tests: move to nvim-treesitter main

--- scripts/minimal_init.lua
+++ scripts/minimal_init.lua
@@ -1,30 +1,25 @@
 vim.cmd([[let &rtp.=','.getcwd()]])
-
+vim.cmd("set rtp+=deps/mini.nvim")
 vim.cmd("set rtp+=./deps/plenary.nvim")
 vim.cmd("set rtp+=./deps/nvim-treesitter")
 
+-- Ensure mini.test is available
+require("mini.test").setup()
+
 -- Install and setup Tree-sitter
-require("nvim-treesitter").setup()
-local required_parsers = { "go", "lua", "markdown", "markdown_inline", "python", "yaml" }
-local installed_parsers = require("nvim-treesitter.info").installed_parsers()
-local to_install = vim.tbl_filter(function(parser)
-  return not vim.tbl_contains(installed_parsers, parser)
-end, required_parsers)
+require("nvim-treesitter").setup({
+  install_dir = "deps/parsers",
+})
 
-if #to_install > 0 then
-  -- fixes 'pos_delta >= 0' error - https://github.com/nvim-lua/plenary.nvim/issues/52
-  vim.cmd("set display=lastline")
-  -- make "TSInstall*" available
-  vim.cmd("runtime! plugin/nvim-treesitter.vim")
-  vim.cmd("TSInstallSync " .. table.concat(to_install, " "))
-end
+require("nvim-treesitter")
+  .install({
+    "go",
+    "lua",
+    "markdown",
+    "markdown_inline",
+    "python",
+    "yaml",
+  })
+  :wait(300000)
 
--- Set up 'mini.test' only when calling headless Neovim (like with `make test`)
-if #vim.api.nvim_list_uis() == 0 then
-  -- Add 'mini.nvim' to 'runtimepath' to be able to use 'mini.test'
-  -- Assumed that 'mini.nvim' is stored in 'deps/mini.nvim'
-  vim.cmd("set rtp+=deps/mini.nvim")
-
-  -- Set up 'mini.test'
-  require("mini.test").setup()
-end
+vim.treesitter.language.register("markdown", "codecompanion")
