4790ea4e413cd828165994a4dc023da728873fa1
Oli <oli.morris@hey.com>
2025-12-19 17:25:43 +0000

    feat(adapters): update Anthropic models (#2556)

--- lua/codecompanion/adapters/http/anthropic.lua
+++ lua/codecompanion/adapters/http/anthropic.lua
@@ -606,46 +606,40 @@ return {
       mapping = "parameters",
       type = "enum",
       desc = "The model that will complete your prompt. See https://docs.anthropic.com/claude/docs/models-overview for additional details and options.",
-      default = "claude-sonnet-4-5-20250929",
+      default = "claude-sonnet-4-5",
       choices = {
-        ["claude-sonnet-4-5-20250929"] = {
-          formatted_name = "Claude Sonnet 4.5",
-          opts = { can_reason = true, has_vision = true },
-        },
-        ["claude-haiku-4-5-20251001"] = {
+        ["claude-haiku-4-5"] = {
           formatted_name = "Claude Haiku 4.5",
           opts = { can_reason = true, has_vision = true },
         },
-
-        ["claude-sonnet-4-20250514"] = {
-          formatted_name = "Claude Sonnet 4",
+        ["claude-opus-4-5"] = {
+          formatted_name = "Claude Opus 4.5",
           opts = { can_reason = true, has_vision = true },
         },
-        ["claude-3-7-sonnet-20250219"] = {
-          formatted_name = "Claude 3.7 Sonnet",
-          opts = { can_reason = true, has_vision = true, has_token_efficient_tools = true },
+        ["claude-sonnet-4-5"] = {
+          formatted_name = "Claude Sonnet 4.5",
+          opts = { can_reason = true, has_vision = true },
         },
-        ["claude-3-5-sonnet-20241022"] = {
-          formatted_name = "Claude Sonnet 3.5",
-          opts = { has_vision = true },
-        },
-        ["claude-opus-4-1-20250805"] = {
+        ["claude-opus-4-1"] = {
           formatted_name = "Claude Opus 4.1",
           opts = { can_reason = true, has_vision = true },
         },
-        ["claude-opus-4-20250514"] = {
+        ["claude-opus-4-0"] = {
           formatted_name = "Claude Opus 4",
           opts = { can_reason = true, has_vision = true },
         },
-        ["claude-3-5-haiku-20241022"] = {
+        ["claude-sonnet-4-0"] = {
+          formatted_name = "Claude Sonnet 4",
+          opts = { can_reason = true, has_vision = true },
+        },
+        ["claude-3-7-sonnet-latest"] = {
+          formatted_name = "Claude Sonnet 3.7",
+          opts = { can_reason = true, has_vision = true, has_token_efficient_tools = true },
+        },
+        ["claude-3-5-haiku-latest"] = {
           formatted_name = "Claude Haiku 3.5",
           opts = { has_vision = true },
         },
-        ["claude-3-opus-20240229"] = {
-          formatted_name = "Claude Opus 3",
-          opts = { has_vision = true },
-        },
-        "claude-2.1",
       },
     },
     ---@type CodeCompanion.Schema
