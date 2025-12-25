669c73ea4e9f2ead86ad876cf0e11c1fd71c5117
cairijun <richard9404@gmail.com>
2025-12-25 14:14:06 +0800

    refactor(mcp): readability improvement

--- lua/codecompanion/mcp/init.lua
+++ lua/codecompanion/mcp/init.lua
@@ -3,23 +3,23 @@ local Client = require("codecompanion.mcp.client")
 local M = {}
 
 ---@class CodeCompanion.MCP.ToolOverride
----@field opts table?
+---@field opts? table
 ---@field enabled nil | boolean | fun(): boolean
----@field system_prompt string?
----@field output table<string, any>?
----@field timeout_ms integer?
+---@field system_prompt? string
+---@field output? table<string, any>
+---@field timeout_ms? integer
 
 ---@class CodeCompanion.MCP.ServerConfig
 ---@field cmd string[]
----@field env table<string, string>?
+---@field env? table<string, string>
 ---@field server_instructions nil | string | fun(orig_server_instructions: string): string
----@field default_tool_opts table<string, any>?
----@field tool_overrides table<string, CodeCompanion.MCP.ToolOverride>?
----@field roots nil | fun(): { name: string?, uri: string }[]
----@field register_roots_list_changed fun(notify: fun())?
+---@field default_tool_opts? table<string, any>
+---@field tool_overrides? table<string, CodeCompanion.MCP.ToolOverride>
+---@field roots? fun(): { name?: string, uri: string }[]
+---@field register_roots_list_changed? fun(notify: fun())
 
 ---@class CodeCompanion.MCPConfig
----@field servers table<string, CodeCompanion.MCP.ServerConfig>?
+---@field servers? table<string, CodeCompanion.MCP.ServerConfig>
 
 ---@type table<string, CodeCompanion.MCP.Client>
 local clients = {}
@@ -35,7 +35,7 @@ function M.start_all_if_not_started()
   end
 
   for _, client in pairs(clients) do
-    client:start_if_not_started()
+    client:start()
   end
 end
 
