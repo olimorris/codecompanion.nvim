02079fad665894ca07457ac6b2eafe68865ef5e6
Oli Morris <olimorris@users.noreply.github.com>
2026-01-02 16:37:00 +0000

    update

--- scripts/minimal_init.lua
+++ scripts/minimal_init.lua
@@ -6,4 +6,22 @@ vim.cmd("set rtp+=deps/nvim-treesitter")
 -- Ensure mini.test is available
 require("mini.test").setup()
 
+-- Install and setup Tree-sitter
+require("nvim-treesitter").setup({
+  install_dir = "deps/parsers",
+})
+
+local ok, err_or_ok = require("nvim-treesitter")
+  .install({
+    "lua",
+    "markdown",
+    "markdown_inline",
+    "yaml",
+  }, { summary = true, max_jobs = 10 })
+  :wait(1800000)
+
+if not ok then
+  print("ERROR: ", err_or_ok)
+end
+
 vim.treesitter.language.register("markdown", "codecompanion")
