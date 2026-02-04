335914d0d9f89be83867c5dbf50fc11a950b76cc
Oli Morris <olimorris@users.noreply.github.com>
2026-02-04 12:27:22 +0000

    wip

--- lua/codecompanion/providers/completion/cmp/setup.lua
+++ lua/codecompanion/providers/completion/cmp/setup.lua
@@ -6,19 +6,19 @@ vim.api.nvim_create_autocmd("FileType", {
   pattern = "codecompanion",
   callback = function()
     local completion = "codecompanion.providers.completion.cmp"
+    cmp.register_source("codecompanion_acp_commands", require(completion .. ".acp_commands").new(config))
+    cmp.register_source("codecompanion_editor_context", require(completion .. ".editor_context").new())
     cmp.register_source("codecompanion_models", require(completion .. ".models").new(config))
     cmp.register_source("codecompanion_slash_commands", require(completion .. ".slash_commands").new(config))
-    cmp.register_source("codecompanion_acp_commands", require(completion .. ".acp_commands").new(config))
     cmp.register_source("codecompanion_tools", require(completion .. ".tools").new(config))
-    cmp.register_source("codecompanion_editor_context", require(completion .. ".editor_context").new())
     cmp.setup.filetype("codecompanion", {
       enabled = true,
       sources = vim.list_extend({
+        { name = "codecompanion_acp_commands" },
+        { name = "codecompanion_editor_context" },
         { name = "codecompanion_models" },
         { name = "codecompanion_slash_commands" },
-        { name = "codecompanion_acp_commands" },
         { name = "codecompanion_tools" },
-        { name = "codecompanion_editor_context" },
       }, cmp.get_config().sources),
     })
     -- returning true will remove this autocmd
