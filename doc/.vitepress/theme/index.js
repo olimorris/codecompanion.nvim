ae548d0b1d06826393318fbfce687ba540d8e77b
Oli Morris <olimorris@users.noreply.github.com>
2026-04-08 22:37:18 +0100

    docs: make it easy to share with LLMs

--- doc/.vitepress/theme/index.js
+++ doc/.vitepress/theme/index.js
@@ -1,6 +1,14 @@
 import DefaultTheme from "vitepress/theme";
+import CopyOrDownloadAsMarkdownButtons from "vitepress-plugin-llms/vitepress-components/CopyOrDownloadAsMarkdownButtons.vue";
+
 import "./vaporwave.css";
 
 export default {
   extends: DefaultTheme,
+  enhanceApp({ app }) {
+    app.component(
+      "CopyOrDownloadAsMarkdownButtons",
+      CopyOrDownloadAsMarkdownButtons,
+    );
+  },
 };
