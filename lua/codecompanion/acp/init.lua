d49d12dd13387ee355d8a77a871459da5a1f7765
Oli <oli.morris@hey.com>
2026-07-28 17:35:22 +0100

    fix(acp): use `SIGTERM` in place of `SIGKILL` (#3275)
    Closes #3274

--- lua/codecompanion/acp/init.lua
+++ lua/codecompanion/acp/init.lua
@@ -545,7 +545,7 @@ end
 ---Disconnect and clean up the ACP process
 ---@return nil
 function Connection:disconnect()
-  assert(self._state.handle):kill(9)
+  assert(self._state.handle):kill(15)
 end
 
 ---Process the output
