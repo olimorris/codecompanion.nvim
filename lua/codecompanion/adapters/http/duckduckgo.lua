822e0714b429d242057b00f2a0df22772897822d
EvgeniyPeshkov <50870158+EvgeniyPeshkov@users.noreply.github.com>
2026-08-03 02:36:21 +0400

    feat(duckduckgo): headers mimicking browser

--- lua/codecompanion/adapters/http/duckduckgo.lua
+++ lua/codecompanion/adapters/http/duckduckgo.lua
@@ -12,7 +12,18 @@ return {
   url = "https://api.duckduckgo.com/",
   env = {},
   headers = {
-    ["Content-Type"] = "application/json",
+    ["User-Agent"] = "Mozilla/5.0 (X11; Linux x86_64; rv:153.0) Gecko/20100101 Firefox/153.0",
+    ["Accept"] = "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+    ["Accept-Language"] = "en-US,en;q=0.9",
+    ["Accept-Encoding"] = "gzip, deflate, br, zstd",
+    ["Connection"] = "keep-alive",
+    ["Upgrade-Insecure-Requests"] = "1",
+    ["Sec-Fetch-Dest"] = "document",
+    ["Sec-Fetch-Mode"] = "navigate",
+    ["Sec-Fetch-Site"] = "none",
+    ["Sec-Fetch-User"] = "?1",
+    ["Priority"] = "u=0, i",
+    ["TE"] = "trailers",
   },
   schema = {
     model = {
